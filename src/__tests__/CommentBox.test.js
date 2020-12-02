1ebbafe5fd0548c129d7cff853a03b509e208411
Bew <Bew@sysops-MBP.broadband>
2020-12-02 12:08:58 +0000

    Add mount for fullDom test and unmount

--- src/__tests__/CommentBox.test.js
+++ src/__tests__/CommentBox.test.js
@@ -4,9 +4,15 @@ import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox.component';
 
 let wrapped;
+
 beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
