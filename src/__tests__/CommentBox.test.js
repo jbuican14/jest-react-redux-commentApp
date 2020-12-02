0a15fde30a8ca9c9e53c124da46f66e183655b82
Bew <Bew@sysops-MBP.broadband>
2020-12-02 13:38:39 +0000

    simulate the submit form

--- src/__tests__/CommentBox.test.js
+++ src/__tests__/CommentBox.test.js
@@ -27,3 +27,14 @@ it('has a text area that users can type in', () => {
 
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment'); //value is the key from the form
 });
+
+it('clears the textarea after submiting the form', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment 2' },
+  });
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment 2');
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
