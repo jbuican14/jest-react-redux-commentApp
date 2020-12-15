370f9afd6c653bcd8cf038df07a9e929d7943969
Bew <Bew@sysops-MBP.broadband>
2020-12-15 22:41:06 +0000

    add helper method renderHeader()

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import CommentBox from 'components/CommentBox.component';
 import CommentList from 'components/CommentList.component';
 import './styles.scss';
 
-const App = () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {}
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>{' '}
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
 
 export default App;
