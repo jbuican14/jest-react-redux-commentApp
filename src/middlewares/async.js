5a3b01093ac9852fd0753ec29dd8e0f271189605
Bew <Bew@sysops-MBP.broadband>
2020-12-17 13:21:56 +0000

    create a middleware functionality

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,1 +1,13 @@
-export default ({ dispatch }) => (next) => (action) => {};
+export default ({ dispatch }) => (next) => (action) => {
+  debugger;
+  // Check if it has a promise? wait : send the action to the next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  // If promise? get its data & create a new action and dispatch it
+  action.payload.then((response) => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
+};
